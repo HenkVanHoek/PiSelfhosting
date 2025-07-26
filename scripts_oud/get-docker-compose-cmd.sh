3d393395d0aab623159dc593826d1d5e8533603d
hvhoek <hvh@freedom.nl>
2025-07-26 11:18:02 +0200

    scripts_oud moved out of the repository. Moved to a my personal projects location.

--- scripts_oud/get-docker-compose-cmd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# PiSelfhosting Docker Compose Command Helper
-# Location: ${SCRIPTS_DIR}/get-docker-compose-cmd.sh # Escaped
-
-if command -v docker &>/dev/null && docker compose version &>/dev/null; then
-    echo "docker compose" # Docker Compose V2 plugin
-else # Geen fallback naar docker-compose (V1)
-    echo "" # Not found or not V2
-fi
